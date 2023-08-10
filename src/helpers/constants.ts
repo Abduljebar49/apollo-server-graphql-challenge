d68d5b272848bd4e9636dff5aaec93e26ddf4eed
Abduljebar Sani <Abduljebar49@gmail.com>
2023-08-10 20:24:29 +0300

    comment added to constant and helper functions

--- src/helpers/constants.ts
+++ src/helpers/constants.ts
@@ -1,3 +1,4 @@
+//these are login message constants
 export const LOGIN_SUCCESS_MESSAGE = "Logged in successfully";
 export const LOGIN_FAILED_MESSAGE = "Incorrect email or password";
 export const LOGIN_FAILED_TWO_FACTOR_MESSAGE = "Incorrect email or password or secretKey";
--- src/helpers/functions.ts
+++ src/helpers/functions.ts
@@ -4,25 +4,35 @@ import dotenv from 'dotenv';
 import { UserDocument } from '../models/user.js';
 import { GraphQLError } from 'graphql';
 
+//we used dotenv to read from .env file in our root folder
+//where we added our access token and refresh token secrets
 dotenv.config();
 
+//we are reading from .env file
+//our secret help us generate token key and also help us read token back
 const accessTokenSecret = process.env.ACCESS_TOKEN_SECRET;
 const refreshTokenSecret = process.env.REFRESH_TOKEN_SECRET;
 
+//these method help us check password if encrypted password is the same as given password
+//if the given password is correct it returns true
 export const checkPassword = async (password: string, existingPassword: string) => {
     return await bcrypt.compare(password, existingPassword);
 }
 
+//this method is used to generate access token using jsonwebtoken library 
 export const generateAccessToken = (user: UserDocument) => {
-
     return jwt.sign(
+        //we are passing user information to store it on token
         JSON.parse(JSON.stringify(user)),
+        //we are passing secret from .env to create a token
         accessTokenSecret
         ,
+        //here I am setting the time for access token to expire which is 1 hour
         { expiresIn: "1h" }
     );
 }
 
+//this method is used to generate refresh token using jsonwebtoken library 
 export const generateRefreshToken = (user: UserDocument) => {
     return jwt.sign(
         JSON.parse(JSON.stringify(user)),
@@ -31,6 +41,8 @@ export const generateRefreshToken = (user: UserDocument) => {
     );
 }
 
+//this method is used to verify our token if it is valid or not
+//if the given token is valid it returns user data
 export const getUser = (token: string) => {
     try {
         if (token) {
@@ -42,6 +54,8 @@ export const getUser = (token: string) => {
     }
 }
 
+//this method is for protected user routes
+//if there is no valid token given it returns unauthenticated
 export const authorizeUser = (user: UserDocument) => {
     if (!user) {
         throw new GraphQLError('User is not authenticated', {
@@ -52,13 +66,14 @@ export const authorizeUser = (user: UserDocument) => {
         });
     }
 }
-
+//this method encypt our password and generated secret key
 export const encryptPassword = async (password: string) => {
     const salt = await bcrypt.genSalt(10);
     const hashedPassword = await bcrypt.hash(password, salt);
     return hashedPassword;
 }
 
+//this method generated user secret key
 export const generateSecretKey = (length: number) => {
     const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
     let result = '';
